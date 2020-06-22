f921296f049fad907ca63fcd7adb99d4c8c27407
mokshjaswal101 <mj950@snu.edu.in>
2020-06-22 14:10:40 +0530

    animations

--- OmniFood/resources/js/script.js
+++ OmniFood/resources/js/script.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
 
-    $('h1').click(function(){
-        $(this).css('background-color','#ff0000')
+    /* Sticky Nav */
+    $('.js-section').waypoint(function(direction){
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+    /* Scroll Button */
+    $('.scroll-to-plan').click(function(){
+        $('html,body').animate({scrollTop: $('.section-plans').offset().top},1000)
+    })
+
+    $('.scroll-to-start').click(function(){
+        $('html,body').animate({scrollTop: $('.js-section').offset().top},1000)
+    })
+
+    /* Animations on scroll */
+    $('.js-wp-1').waypoint(function(direction){
+        $('.js-wp-1').addClass('animated fadeIn');
     });
 
 });
